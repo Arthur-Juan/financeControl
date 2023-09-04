bde808f8cf8eb179010259b9fe11134af7d6c2ef
Arthur Lima <arthurjuan214@gmail.com>
2023-09-04 13:37:49 -0300

    BaseEntity

---
+++ src/Domain/Entities/BaseEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Entities;
+
+public abstract class BaseEntity
+{
+    protected BaseEntity(){}
+    
+    public Guid Id { get; protected set; }
+    public DateTimeOffset CreatedAt { get; set; }
+    public DateTimeOffset UpdatedAt { get; set; }
+    public DateTimeOffset DeletedAt { get; set; }
+
+}
